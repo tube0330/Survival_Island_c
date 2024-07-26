230d4b5621b75d1be698b57050916e49f5bbb8fc
tube0330 <zien0516@naver.com>
2024-07-26 17:30:22 +0900

    240714_drag, drop 잘못된듯

--- Assets/02_Scripts/Common/InventoryDrop.cs
+++ Assets/02_Scripts/Common/InventoryDrop.cs
@@ -2,20 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using DataInfo;
 
 public class InventoryDrop : MonoBehaviour, IDropHandler
 {
 
-    void Start()
-    {
-        
-    }
-
     public void OnDrop(PointerEventData eventData)
     {
         if (transform.childCount == 0)
-        {
             InventoryDrag.draggingItem.transform.SetParent(transform, false);
-        }
+
+        Item item = InventoryDrag.draggingItem.GetComponent<itemInfo>().C_item;
+
+        GameManager.G_Instance.AddItem(item);
     }
 }
